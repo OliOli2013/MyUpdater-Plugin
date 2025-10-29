4dffc3cc44db05a1230d3c124ab37e1d5213303c
Pawel Pawelek <pawelzarzycki61@gmail.com>
2025-10-29 13:03:40 +0100

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,18 +1,14 @@
 # -*- coding: utf-8 -*-
-# MyUpdater Enhanced - Inicjalizacja wtyczki
-# Ten plik jest wymagany przez Enigma2 do prawidłowego działania wtyczki
+# Prosta inicjalizacja bez cyklicznych importów
 
-from . import plugin_enhanced
+__version__ = "V4 Enhanced"
+__author__ = "Paweł Pawełek"
 
-# Wersja wtyczki
-__version__ = "V5 Enhanced"
-__author__ = "Paweł Pawełek, Sancho, gut"
-__description__ = "MyUpdater Enhanced - kompatybilny z OpenATV/OpenPLI"
-
-# Eksportujemy główną funkcję
+# Importujemy dopiero gdy jest to potrzebne
 def main(session, **kwargs):
-    return plugin_enhanced.main(session, **kwargs)
+    from . import plugin
+    return plugin.main(session, **kwargs)
 
-# Eksportujemy deskryptor wtyczki
 def Plugins(**kwargs):
-    return plugin_enhanced.Plugins(**kwargs)
+    from . import plugin
+    return plugin.Plugins(**kwargs)
